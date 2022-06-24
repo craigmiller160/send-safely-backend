f21d9a55379efd9b17e21f7b45ec64a141cec90f
Craig Miller <craigmiller160@gmail.com>
2022-06-24 12:48:50 -0400

    Extracting arguments

--- src/main/java/io/github/craigmiller160/sendsafely/service/action/FinalizePackageService.java
+++ src/main/java/io/github/craigmiller160/sendsafely/service/action/FinalizePackageService.java
@@ -2,6 +2,7 @@ package io.github.craigmiller160.sendsafely.service.action;
 
 import io.github.craigmiller160.sendsafely.log.Logger;
 import io.github.craigmiller160.sendsafely.model.ArgumentKey;
+import io.github.craigmiller160.sendsafely.model.arguments.FinalizePackageArguments;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,15 @@ public class FinalizePackageService implements ActionService {
   private final Logger logger;
 
   @Override
-  public void perform(Map<ArgumentKey, String> arguments) throws Exception {}
+  public void perform(final Map<ArgumentKey, String> arguments) throws Exception {}
+
+  private FinalizePackageArguments extractArguments(final Map<ArgumentKey, String> arguments) {
+    final var extractedArguments =
+        new FinalizePackageArguments(
+            arguments.get(ArgumentKey.API_KEY),
+            arguments.get(ArgumentKey.API_SECRET),
+            arguments.get(ArgumentKey.PACKAGE_ID));
+    extractedArguments.validate();
+    return extractedArguments;
+  }
 }
