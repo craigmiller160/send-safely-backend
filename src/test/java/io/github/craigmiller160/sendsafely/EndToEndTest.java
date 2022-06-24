a3c7ebf39f3d947aec10bce3bcc414e10f7b8e7c
Craig Miller <craigmiller160@gmail.com>
2022-06-24 11:54:24 -0400

    Cleanup

--- src/test/java/io/github/craigmiller160/sendsafely/EndToEndTest.java
+++ src/test/java/io/github/craigmiller160/sendsafely/EndToEndTest.java
@@ -1,6 +1,5 @@
 package io.github.craigmiller160.sendsafely;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,13 +8,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest(classes = TestContainerConfig.class)
 @ExtendWith(SpringExtension.class)
 public class EndToEndTest {
-  private String apiKey;
-  private String apiSecret;
-
-  @BeforeEach
-  public void setup() {
-    System.out.println("KEY: " + System.getenv("SS_API_KEY"));
-  }
 
   @Test
   public void createAndTearDownPackage() {
