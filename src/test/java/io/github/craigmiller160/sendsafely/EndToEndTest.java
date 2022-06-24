c71cf6c0dde0b7de5117bc15348aef034d23ca7d
Craig Miller <craigmiller160@gmail.com>
2022-06-24 11:48:32 -0400

    Stubbed out tests

---
+++ src/test/java/io/github/craigmiller160/sendsafely/EndToEndTest.java
@@ -0,0 +1,20 @@
+package io.github.craigmiller160.sendsafely;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class EndToEndTest {
+  @Test
+  public void createAndTearDownPackage() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  public void createAndFinalizePackage() {
+    throw new RuntimeException();
+  }
+}
