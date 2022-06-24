3bf73c139db6f856348a2adbbd94734f6096dbd4
Craig Miller <craigmiller160@gmail.com>
2022-06-24 09:49:45 -0400

    Adding more dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 plugins {
     id("java")
     id("com.diffplug.spotless") version "6.6.1"
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "io.github.craigmiller160"
@@ -19,8 +21,12 @@ repositories {
 
 dependencies {
     val junitVersion = "5.8.2"
+    val sendSafelyVersion = "3.1.7"
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    implementation("com.sendsafely:sendsafely-java-api:$sendSafelyVersion")
+    implementation("org.springframework.boot:spring-boot-starter")
 }
 
 tasks {
