f48ea0f212e3f8453c83e6440cdb90840c52f14e
Craig Miller <craigmiller160@gmail.com>
2022-06-24 10:57:04 -0400

    Cleanup

--- src/main/java/io/github/craigmiller160/sendsafely/service/action/ActionService.java
+++ src/main/java/io/github/craigmiller160/sendsafely/service/action/ActionService.java
@@ -4,7 +4,7 @@ import io.github.craigmiller160.sendsafely.model.ArgumentKey;
 import java.util.Map;
 
 public interface ActionService {
-  public static final String SEND_SAFELY_URL = "https://app.sendsafely.com";
+  String SEND_SAFELY_URL = "https://app.sendsafely.com";
 
   void perform(final Map<ArgumentKey, String> arguments) throws Exception;
 }
