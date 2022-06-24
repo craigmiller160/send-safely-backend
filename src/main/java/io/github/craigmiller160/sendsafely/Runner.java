46eac4dd2f3facf3a50c7d3b74c925ecd18ed225
Craig Miller <craigmiller160@gmail.com>
2022-06-24 09:52:59 -0400

    Added runner

---
+++ src/main/java/io/github/craigmiller160/sendsafely/Runner.java
@@ -0,0 +1,11 @@
+package io.github.craigmiller160.sendsafely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runner {
+  public static void main(final String[] args) {
+    SpringApplication.run(Runner.class, args);
+  }
+}
